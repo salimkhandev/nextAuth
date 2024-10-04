672a8e3f53da67a8986b1dd2e391a93a36b0e108
salimkhandev <salimt450s@gmail.com>
2024-10-04 17:56:08 +0500

    added google auth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,13 +1,18 @@
 import NextAuth from 'next-auth'
 import GithubProvider from "next-auth/providers/github"
 
-const handler= NextAuth({
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
     providers: [
-        // OAuth authentication providers...
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        }),
         GithubProvider({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET,
-        }),
+        })
     ]
 })
-export {handler as GET , handler as POST};
+export { handler as GET, handler as POST };
